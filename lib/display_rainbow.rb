6bc5c6d186ba63437932446450ada7bf1bbfbcfd
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-06-18 23:37:07 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 # Write your #display_rainbow method here
 
 def display_rainbow(colors)
-  return colors.map {|c| "#{c[0].upcase}: #{c}"}.join(", ").to_s + "\n"
+  puts colors.map {|c| "#{c[0].upcase}: #{c}"}.join(", ").to_s + "\n"
 end
