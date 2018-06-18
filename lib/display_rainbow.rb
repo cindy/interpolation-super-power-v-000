e69299912f4f791526123fea9520092ac14e32e1
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-06-18 23:33:30 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,8 +1,8 @@
 # Write your #display_rainbow method here
 
 def display_rainbow(colors)
-  string = colors.map {|c| "#{c[0].upcase}: #{c}"}.join(", ").to_s + "\n"
+  puts colors.map {|c| "#{c[0].upcase}: #{c}"}.join(", ").to_s + "\n"
 end
 
 
-print display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']) == "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet\n"
+print display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
