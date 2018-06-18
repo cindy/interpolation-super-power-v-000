11f4c23c2af32627d3858f3b92db709e6a621c2e
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-06-18 23:16:04 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,10 +1,4 @@
 # Write your #display_rainbow method here
-with the following invocation: `display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])`.
-3. `#display_rainbow` should print out the colors of the rainbow in the following format: `"R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"` by reading from the array passed in as an argument. (For this lab it is OK to hardcode the uppercase letters.)
-4. It should accept an array containing the colors as an argument.
-5. Run `learn` locally until you pass.
-6. Submit the lab.
-
 
 def display_rainbow(colors)
   for i in colors:
