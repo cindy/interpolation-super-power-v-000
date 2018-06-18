683b30fc080fa7995a62efcd9dcceeaddea6e01f
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-06-18 23:32:39 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -5,4 +5,4 @@ def display_rainbow(colors)
 end
 
 
-print display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+print display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']) == ""R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet\n""
